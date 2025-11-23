36e13577b28f021bdce1316f90272ad97f2ce785
Sumit Shaw <sumit.shawsh2009@gmail.com>
2025-11-23 14:20:39 +0530

    First deployement

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -49,6 +49,8 @@ app.post("/specific", async (req, res) => {
 
 
 
+
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
+module.exports = app;
